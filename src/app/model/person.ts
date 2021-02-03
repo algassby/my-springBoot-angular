814af712001beb22633e4affaafa9d47dbc4b0ff
algassby <algassby@hotmail.com>
2021-02-03 16:01:49 +0100

    search by

--- src/app/model/person.ts
+++ src/app/model/person.ts
@@ -10,7 +10,8 @@ export class Person {
 
      roles: Role [] =[] ;
    // public password:String = "";
-    constructor(public id:number, public nom:String, public password:String, public fonction:String,public tel: number, public sexe:String,public age:number){
+    constructor(public id:number, public nom:String, public password:String, public fonction:String,public tel: number,
+         public sexe:String,public age:number,public email:String, public username:String){
         this.roles =[];
         
         
--- src/app/model/user.ts
+++ src/app/model/user.ts
@@ -1,7 +1,8 @@
 export class User {
 
     constructor(public id:number, public nom:String, public password:String, public fonction:String,
-        public tel: number, public sexe:String,public age:number, public role:any[]){
+        
+        public tel: number, public sexe:String,public age:number,public email:String, public username:String, public role:any[]){
         
 }
 
